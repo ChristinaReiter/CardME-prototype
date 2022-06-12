0a54edbe72ef8115abee0d598b3c1dbd733a2a08
Justus <justusschoenborn@gmx.de>
2022-06-12 18:05:19 +0200

    Theming Start (font not working yet)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,18 +4,35 @@ import Cards from "./components/Cards";
 import Create from "./components/Create";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import { createTheme, ThemeProvider} from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#a7cda7'
+    },
+    secondary: {
+      main: '#0a5108'
+    },
+    typography: {
+      fontFamily: 'Annie+Use+Your+Telescope'
+    }
+  }
+})
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route exact path="/cards" element={<Cards />} />
-          <Route exact path="/create" element={<Create />} />
-        </Routes>
-      </BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route exact path="/cards" element={<Cards />} />
+            <Route exact path="/create" element={<Create />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
     </div>
   );
 }
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { Typography, Button } from "@mui/material";
+
+
 
 const Home = () => {
   return (
     <div>
-      <p>Home</p>
+      <Typography variant="h6" color="primary">
+        Home Page
+      </Typography>
+
+      <Button variant="contained" color="secondary">
+        Browse Cards
+      </Button>
+      <Button variant="contained" color="secondary">
+        Create your own Card
+      </Button>
     </div>
   );
 };
