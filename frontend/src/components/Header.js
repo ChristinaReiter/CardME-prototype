69306058a36f51e5edfe75b90f537a0a5b211771
ge34wij <ge34wij@mytum.de>
2022-06-13 13:30:23 +0200

    Toolbar initial erstellt

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,19 +1,43 @@
 import React from "react";
 import logo from "./../assets/images/logo_transparent.png";
 import { NavLink } from "react-router-dom";
+import { AppBar } from "@mui/material";
+import { Toolbar } from "@mui/material";
+import { Typography } from "@mui/material";
 
 const Header = () => {
   return (
-    <nav>
+    <AppBar position="static">
+      <Toolbar>
         <NavLink to="/">
           <img src={logo} alt="logo" width="15%"></img>
         </NavLink>
-        <NavLink to="/cards">Every Day</NavLink>
-        <NavLink to="/cards">Occasion</NavLink>
-        <NavLink to="/cards">Seasonal</NavLink>
-        <NavLink to="/create">Create</NavLink>
-    </nav>
+        <NavLink to="/cards">
+          <Typography>Every Day</Typography>
+        </NavLink>
+        <NavLink to="/cards">
+          <Typography>Occasion</Typography>
+        </NavLink>
+        <NavLink to="/cards">
+          <Typography>Seasonal</Typography>
+        </NavLink>
+        <NavLink to="/create">
+          <Typography>Create</Typography>
+        </NavLink>
+      </Toolbar>
+    </AppBar>
   );
+
+  //   <nav>
+  //       <NavLink to="/">
+  //         <img src={logo} alt="logo" width="15%"></img>
+  //       </NavLink>
+  //       <NavLink to="/cards">Every Day</NavLink>
+  //       <NavLink to="/cards">Occasion</NavLink>
+  //       <NavLink to="/cards">Seasonal</NavLink>
+  //       <NavLink to="/create">Create</NavLink>
+  //   </nav>
+  // );
 };
 
 export default Header;
