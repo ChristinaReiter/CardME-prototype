6c4929e1ee6b07c1335a578091b5fd6eec3eed43
Viola Stumpf <violalaura@gmx.de>
2022-06-19 20:19:38 +0200

    added the card images from the mockup, (added the url tag in the database) and worked on dynamically placing the card images (so that every card has a unique image based on the url property in the database) changed Paper UI for card UI and need to work on the design again

--- frontend/src/components/Cards.js
+++ frontend/src/components/Cards.js
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { Typography, Button, Box, Grid, Paper, Card, CardActions, CardContent} from "@mui/material";
+import { Typography, Button, Box, Grid, Paper, Card, CardActions, CardContent, CardMedia} from "@mui/material";
 import { styled } from '@mui/material/styles';
 import CardService from "../services/CardService";
-import CardPlaceholder from "./../assets/images/card_placeholder.jpg";
+import CardPlaceholder from "./../assets/images/happymothersday.jpg";
 import { margin } from "@mui/system";
+import image01 from '../assets/images/happymothersday.jpg'
+import image02 from '../assets/images/flowerywishes.jpg'
+import image03 from '../assets/images/flowerpastel.jpg'
+import image04 from '../assets/images/imoustacheyou.png'
+
 
 
 
@@ -29,26 +34,27 @@ const Cards = () => {
     color: theme.palette.text.secondary,
   }));
 
+let images = new Map();
+images.set('happymothersday', image01);
+images.set('flowerywishes', image02);
+images.set('pastellflowers', image03);
+images.set('imoustacheyou', image04);
           
   return (
     <div>
       <Box sx={{ width: '100%', margin:'90px 30px 30px 30px'}}>
         <Typography variant="h4">All Cards:</Typography>
         <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} sx={{margin:'20px 10px 10px 10px'}}>
-          {products.map((product, i) => (
+          {products.map((product) => (
             <Grid item xs={3}>
-              <Item>
-                <img 
-                  alt = "Card-Preview"
-                  source={CardPlaceholder}
-                  width ="100%"
-                  sx={{ display: "block", height: "auto" }}
+              <Card>
+                <CardMedia
+                  component="img"
+                  height="194"
+                  image={images.get(product.url)}
+                  alt="Card-Preview"
                 />
-                <p key={product._id}>
-                  <Typography variant="h5" gutterBottom="true">{product.title}</Typography>
-                  <Typography variant="h10" gutterBottom="true">by {product.designer}</Typography>
-                </p>
-              </Item>
+              </Card>
             </Grid>
           ))}
         </Grid>
@@ -58,3 +64,17 @@ const Cards = () => {
 };
 
 export default Cards;
+
+
+{/*<Item>
+                <img 
+                  alt = "Card-Preview"
+                  source= {"../assets/images/" + product.url}
+                  width ="100%"
+                  sx={{ display: "block", height: "auto" }}
+                />
+                <p key={product._id}>
+                  <Typography variant="h5" gutterBottom="true">{product.title}</Typography>
+                  <Typography variant="h10" gutterBottom="true">by {product.designer}</Typography>
+                </p>
+</Item>*/}
