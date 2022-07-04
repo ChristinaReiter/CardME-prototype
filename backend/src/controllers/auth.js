f60b45d9a59edc4794b21ce9640a86bf0372a903
Justus <justusschoenborn@gmx.de>
2022-07-04 21:50:36 +0200

    Fixed Login+ Hashed PWs+ JWT Token (only assigned)

--- backend/src/controllers/auth.js
+++ backend/src/controllers/auth.js
@@ -1,17 +1,20 @@
 const User = require("../models/user");
 const Account = require('../models/account');
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 
 const register = async (req, res) => {
     console.log(req.body)
     try{
+      const securePassword = await bcrypt.hash(req.body.password, 8);
       const user = await User.create({
         email: req.body.email,
         name: req.body.name,
          })
       const account = await Account.create({
-        user: user._id,
-        password: req.body.password,
+        _id: user._id,
+        password: securePassword,
       })
       return res.json({response: 'success'})
     
@@ -28,30 +31,27 @@ const register = async (req, res) => {
     try{
       const user = await User.findOne({email: req.body.email})
       const account = await Account.findOne({
-      user: user._id,
-      password: req.body.password,    
+        _id: user._id, 
     })
-
-      if (account){
-        res.json({status: 'ok', account: true})
-
-        /* const token = jwt.sign({
-          id: user._id,
-          email: user.email,
-          },
-          "secret123"  // better secret needed
-        ) */
-
-
+    console.log(account.password)
+    const isPasswordValid = await bcrypt.compare(req.body.password, account.password)
+     
+    if (isPasswordValid){
+        const token = jwt.sign({
+        _id: account._id,
+        },
+        "secret123"  // better secret needed
+        ) 
+        return res.json({status: 'ok', account: token})
       
-      }else {
-        res.json({status: 'error', message: 'User not found'})
-      }
+     }else {
+        return res.json({status: 'error', message: 'Password Invalid'})
+     }
     } catch (err) {
-    return res.status(404).json({
-      error: "User Not Found",
-      message: err.message,
-      });
+        return res.status(404).json({
+          error: "Account Not Found",
+          message: err.message,
+        });
     }
 };  
 
