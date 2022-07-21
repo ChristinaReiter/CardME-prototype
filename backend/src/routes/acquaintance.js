a0ee72cb95163822a31a7751381a739da6ccf1c1
Justus <justusschoenborn@gmx.de>
2022-07-21 23:35:22 +0200

    Delete Aquaintances (still have to refresh page)

--- backend/src/routes/acquaintance.js
+++ backend/src/routes/acquaintance.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const { secured } = require("../middleware/authMW");
 
 const AcquaintanceController = require("../controllers/acquaintance");
-//delete and put need ids!!
+
 router.route("/").get( secured, AcquaintanceController.getAcquaintances).post( secured,  AcquaintanceController.setAcquaintance)
 
-router.route("/:id").put(/* secured, */ AcquaintanceController.updateAcquaintance).delete( secured, AcquaintanceController.deleteAcquaintance);
+router.route("/:id").put( secured,  AcquaintanceController.updateAcquaintance).delete( secured, AcquaintanceController.deleteAcquaintance);
 
 module.exports = router;  
